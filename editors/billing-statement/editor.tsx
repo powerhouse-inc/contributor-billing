2842749631d21234050dc0087298107e215d2b49
liberuum <liberom@proton.me>
2025-06-12 21:57:40 +0100

    wip billing-statement editor

--- editors/billing-statement/editor.tsx
+++ editors/billing-statement/editor.tsx
@@ -1,14 +1,23 @@
+import { ChangeEvent, useState } from "react";
 import type { EditorProps } from "document-model";
 import {
   type BillingStatementDocument,
   actions,
 } from "../../document-models/billing-statement/index.js";
+import { generateId } from "document-model";
 import { CurrencyForm } from "../invoice/components/currencyForm.js";
-import LineItems from "./components/lineItems.js";
+import { Textarea } from "@powerhousedao/document-engineering";
+import ObjectSetTableComponent from "./components/objectSetTable.js";
 
 export type IProps = EditorProps<BillingStatementDocument>;
 
 export default function Editor(props: IProps) {
+  const { document: doc, dispatch } = props;
+  const state = doc.state.global;
+  // console.log("global state", state);
+
+  const [notes, setNotes] = useState(state.notes ?? "");
+
   return (
     <div className="editor-container">
       {/* Header */}
@@ -34,12 +43,53 @@ export default function Editor(props: IProps) {
           <span className="mr-2 pt-2">Currency: </span>
           <CurrencyForm
             currency={props.document.state.global.currency}
-            handleInputChange={() => {}}
+            handleInputChange={(e: ChangeEvent<HTMLInputElement>) => {
+              dispatch(actions.editBillingStatement({ currency: e.target.value }));
+            }}
           />
         </div>
       </div>
       <div className="">
-        <LineItems />
+        <ObjectSetTableComponent state={state} dispatch={dispatch} />
+      </div>
+      <button
+          className="bg-blue-500 text-white px-4 py-2 rounded-md text-xs"
+          onClick={() => {
+            dispatch(
+              actions.addLineItem({
+                id: generateId(),
+                description: "Dummy Line Item",
+                quantity: 5,
+                unit: "HOUR",
+                unitPriceCash: 2,
+                unitPricePwt: 1,
+                totalPriceCash: 5 * 2,
+                totalPricePwt: 5 * 1,
+              })
+            );
+          }}
+        >
+          Add Dummy Line Item
+        </button>
+      <div className="mt-6 p-2">
+        <Textarea
+          label="Notes"
+          placeholder="Add notes"
+          autoExpand={false}
+          rows={4}
+          multiline={true}
+          value={notes}
+          onBlur={(e) => {
+            const newValue = e.target.value;
+            if (newValue !== state.notes) {
+              dispatch(actions.editBillingStatement({ notes: newValue }));
+            }
+          }}
+          onChange={(e) => {
+            setNotes(e.target.value);
+          }}
+          className="p-2 mb-4"
+        />
       </div>
     </div>
   );
