4b636bd2b0a6baa9d6adc263cc326d3843acc608
liberuum <liberom@proton.me>
2025-06-16 17:50:34 +0100

    core-dev tailwind fix + style adjustment

--- editors/billing-statement/editor.tsx
+++ editors/billing-statement/editor.tsx
@@ -78,7 +78,7 @@ export default function Editor(props: IProps) {
         Add Dummy Line Item
       </button> */}
       {/* Text Area and Totals Table */}
-      <div className="grid sm:grid-cols-2 two-column-grid">
+      <div className="grid sm:grid-cols-2">
         <div className="mt-6 p-2 two-column-grid:mt-2">
           <Textarea
             label="Notes"
@@ -99,7 +99,7 @@ export default function Editor(props: IProps) {
             className="p-2 mb-4"
           />
         </div>
-        <div className="mt-6 p-2 flex justify-center items-center">
+        <div className="p-2 flex justify-center items-center sm:mt-6">
           <table className="border border-gray-300">
             <thead>
               <tr className="bg-gray-100">
