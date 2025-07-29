19e4f9c6640ee40c474c6056e696c6ac85b899eb
liberuum <liberom@proton.me>
2025-07-29 11:59:23 +0100

    f

--- document-models/integrations/gen/utils.ts
+++ document-models/integrations/gen/utils.ts
@@ -15,7 +15,34 @@ import {
 } from "./types.js";
 import { reducer } from "./reducer.js";
 
-export const initialGlobalState: IntegrationsState = {};
+export const initialGlobalState: IntegrationsState = {
+  gnosisSafe: {
+    safeAddress: "",
+    signerPrivateKey: "",
+  },
+  googleCloud: {
+    projectId: "",
+    location: "",
+    processorId: "",
+    keyFile: {
+      type: "",
+      project_id: "",
+      private_key_id: "",
+      private_key: "",
+      client_email: "",
+      client_id: "",
+      auth_uri: "",
+      token_uri: "",
+      auth_provider_x509_cert_url: "",
+      client_x509_cert_url: "",
+      universe_domain: "",
+    },
+  },
+  requestFinance: {
+    apiKey: "",
+    email: "",
+  },
+};
 export const initialLocalState: IntegrationsLocalState = {};
 
 const utils: DocumentModelUtils<IntegrationsDocument> = {
