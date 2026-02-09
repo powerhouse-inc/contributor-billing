06c7b26eec58d1941e0e3959fec5f597c48a7d3b
liberuum <liberom@proton.me>
2026-02-09 16:41:50 +0000

    remove obsolete subgraph

--- subgraphs/index.ts
+++ subgraphs/index.ts
@@ -1,5 +1,4 @@
 export * as InvoiceAddonSubgraph from "./invoice-addon/index.js";
 export * as AccTxsAddonSubgraph from "./acc-txs-addon/index.js";
 export * as BudgetStatementsSubgraph from "./budget-statements/index.js";
-export * as ExpenseReportSubgraph from "./expense-report/index.js";
 export * as ResourcesServicesSubgraph from "./resources-services/index.js";
